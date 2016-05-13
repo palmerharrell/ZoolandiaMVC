c8079ff398312c73b5d204f575dd6e2c1868691f
Palmer Harrell <palmerharrell@me.com>
2016-05-13 10:09:20 -0500

    First commit: set up ASP.net MVC project

---
+++ ZoolandiaMVC/Controllers/ZoolandiaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZoolandiaMVC.Controllers
+{
+    public class ZoolandiaController : Controller
+    {
+        // GET: Zoolandia
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ZoolandiaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZoolandiaMVC.MvcApplication" Language="C#" %>
